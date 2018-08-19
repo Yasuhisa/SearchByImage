bcdc91ac6ef661f81181d3c7d3358ee73af1d1d1
arakawa-yasuhisa <arakawa.yasuhisa@gmail.com>
2018-08-19 18:46:59 +0900

    Add Resources

--- SearchByImage/SearchByImage/ViewController/CameraViewController.swift
+++ SearchByImage/SearchByImage/ViewController/CameraViewController.swift
@@ -2,7 +2,7 @@
 //  CameraViewController.swift
 //  SearchByImage
 //
-//  Created by yasuhisa.arakawa on 2018/07/31.
+//  Created by arakawa.yasuhisa on 2018/08/19.
 //  Copyright © 2018年 yasuhisa.arakawa. All rights reserved.
 //
 
@@ -12,8 +12,6 @@ class CameraViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +19,4 @@ class CameraViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
